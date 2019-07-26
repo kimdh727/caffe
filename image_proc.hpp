cbe0a5b6cb69dc3028720c77abef4d5e43bad9e2
socmgr <kimdh727@naver.com>
2019-07-26 10:44:07 +0900

    image_process conv, pool

--- image_proc.hpp
+++ image_proc.hpp
@@ -43,7 +43,7 @@ Mat conv2D(Mat image, Mat filter,int padding_size, int stride=1){
     for(int i=0; i<result.rows; i++){
         for(int j=0; j<result.cols; j++){
             double temp = 0;
-            for(int k=0; k<filter.rows; k++){     // convolution
+            for(int k=0; k<filter.rows; k++){           // Convolution
                 for(int l=0; l<filter.cols; l++){
                         temp += pad.at<uchar>(i*stride+k,j*stride+l) * filter.at<double>(k,l);           
                 }
@@ -66,13 +66,13 @@ Mat max_pooling(Mat image, int pooling_size=2, int stride=2){
     Mat result((image.rows-pooling_size)/stride + 1, (image.cols-pooling_size)/stride + 1, image.type());
     
     for(int i=0; i<result.rows; i++){
-        for(int j=0; j<result.cols; j++){           
-            result.at<uchar>(i,j) = 0;              // Init
+        for(int j=0; j<result.cols; j++){            
+                result.at<uchar>(i,j) = 0;                      // Init
 
-            for(int k=0; k<pooling_size; k++){      // convolution
+            for(int k=0; k<pooling_size; k++){                  // Convolution
                 for(int l=0; l<pooling_size; l++){
                     if(result.at<uchar>(i,j)<image.at<uchar>(i*stride + k,j*stride + l))
-                        result.at<uchar>(i,j) = image.at<uchar>(i*stride + k,j*stride + l);            
+                        result.at<uchar>(i,j) = image.at<uchar>(i*stride + k,j*stride + l);       
                 }
             }
 
@@ -91,15 +91,15 @@ Mat average_pooling(Mat image, int pooling_size=2, int stride=2){
     
     for(int i=0; i<result.rows; i++){
         for(int j=0; j<result.cols; j++){
-            int temp = 0;
+            int temp = 0;                               // Init
 
-            for(int k=0; k<pooling_size; k++){     // convolution
+            for(int k=0; k<pooling_size; k++){          // Convolution
                 for(int l=0; l<pooling_size; l++){
-                        temp += image.at<uchar>(i*stride + k, j*stride + l);
+                    temp += image.at<uchar>(i*stride + k, j*stride + l);           
                 }
             }
-            result.at<uchar>(i,j) = temp/(pooling_size*pooling_size);       
-            delete(temp);
+
+            result.at<uchar>(i,j) = temp/(pooling_size*pooling_size);
         }
     }
 
@@ -143,15 +143,15 @@ void comp(Mat img1, Mat img2, int print=0){
     for (int i=0; i<img1.rows; i++){
         for (int j=0; j<img1.cols; j++){
             for (int ch=0; ch<3; ch++){
-                if (abs(img1.at<double>(i,j)[ch] - img2.at<double>(i,j)[ch])>0){
+                if (abs(img1.at<Vec3b>(i,j)[ch] - img2.at<Vec3b>(i,j)[ch])>1){
                     //if(i!=0&&i!=img1.rows-1&&j!=0&&j!=img1.cols-1){
                     if (print){
                         cout << "location\n";
                         cout << i << "\t" << j << "\tch:" << ch << endl;
                         cout << "img1 info\t";
-                        cout << (int)img1.at<double>(i,j)[ch] << endl;
+                        cout << (int)img1.at<Vec3b>(i,j)[ch] << endl;
                         cout << "img2 info\t";
-                        cout << (int)img2.at<double>(i,j)[ch] << endl << endl;
+                        cout << (int)img2.at<Vec3b>(i,j)[ch] << endl << endl;
                     }//}
                     //if(i!=0&&i!=img1.rows-1&&j!=0&&j!=img1.cols-1)
                         cnt++;
